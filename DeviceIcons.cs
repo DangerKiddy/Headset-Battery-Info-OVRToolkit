b6e9a98b06e78e25104f708a6700b550c02c6edc
DangerKiddy <42438297+DangerKiddy@users.noreply.github.com>
2023-12-02 00:32:20 +0700

    Добавьте файлы проекта.

---
+++ DeviceIcons.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HeadsetBatteryInfo
+{
+    internal class DeviceIcons
+    {
+        public struct HeadsetIcons
+        {
+            public byte[] headset;
+            public byte[] headsetCharging;
+
+            public byte[] leftController;
+            public byte[] rightController;
+        }
+
+        public static HeadsetIcons Unknown;
+        public static HeadsetIcons Pico;
+        public static HeadsetIcons Meta;
+
+        private static Dictionary<Company, HeadsetIcons> deviceIcons = new Dictionary<Company, HeadsetIcons>();
+        public static void Init()
+        {
+            InitDefault();
+            InitPico();
+            InitMeta();
+
+            deviceIcons[Company.Unknown] = Unknown;
+            deviceIcons[Company.Pico] = Pico;
+            deviceIcons[Company.Meta] = Meta;
+        }
+
+        private static void InitPico()
+        {
+            Pico = new HeadsetIcons();
+
+            Pico.headset = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/pico/headset.png");
+            Pico.headsetCharging = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/pico/headset_charging.png");
+
+            Pico.leftController = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/pico/left_controller.png");
+            Pico.rightController = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/pico/right_controller.png");
+        }
+
+        private static void InitMeta()
+        {
+            Meta = new HeadsetIcons();
+
+            Meta.headset = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/meta/headset.png");
+            Meta.headsetCharging = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/meta/headset_charging.png");
+
+            Meta.leftController = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/meta/left_controller.png");
+            Meta.rightController = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/meta/right_controller.png");
+        }
+
+        private static void InitDefault()
+        {
+            Unknown = new HeadsetIcons();
+
+            Unknown.headset = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/unknown/headset.png");
+            Unknown.headsetCharging = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/unknown/headset_charging.png");
+            Unknown.leftController = Unknown.rightController = File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/unknown/controller.png");
+        }
+
+        public static byte[] GetDeviceIcon(DeviceType device, Company company, bool isCharging = false)
+        {
+            var icons = deviceIcons[company];
+
+            switch (device)
+            {
+                case DeviceType.Headset:
+                    return isCharging ? icons.headsetCharging : icons.headset;
+
+                case DeviceType.ControllerLeft:
+                    return icons.leftController;
+
+                case DeviceType.ControllerRight:
+                    return icons.rightController;
+
+                default:
+                    return icons.headset;
+            }
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System;
+using OVRToolkit.Modules;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace HeadsetBatteryInfo
+{
+    public enum Company
+    {
+        Unknown = -1,
+
+        Pico,
+        Meta
+    }
+    public enum DeviceType
+    {
+        Headset,
+        ControllerLeft,
+        ControllerRight,
+    }
+    public struct DevicePacket
+    {
+        public DeviceType device;
+        public bool isCharging;
+        public int batteryLevel;
+        public Company company;
+    }
+
+    public class OVRToolkitModule : Module
+    {
+
+        private const int port = 28093;
+
+        private UdpClient udp;
+        private bool udpCreated = false;
+
+        Dictionary<string, object> contents = new Dictionary<string, object>();
+        Dictionary<DeviceType, DevicePacket> packets = new Dictionary<DeviceType, DevicePacket>();
+        private static Company company = Company.Unknown;
+
+        public override void Start()
+        {
+            InitModule("Headset Battery Info", null);
+
+            try
+            {
+                if (udp == null)
+                    udp = new UdpClient(port);
+
+                udpCreated = true;
+            }
+            catch
+            {
+                contents.Add("HBI_Error", new Header("Failed to create listen socket!"));
+
+                udpCreated = false;
+            }
+
+            SetContents(contents.Values.ToArray());
+            ListenForData();
+        }
+
+        private async void ListenForData()
+        {
+            while (true)
+            {
+                var data = await udp.ReceiveAsync();
+
+                if (data != null && data.Buffer.Length > 0)
+                {
+                    var packet = GetPacketFromBytes(data.Buffer);
+
+                    packets[packet.device] = packet;
+                }
+
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+            }
+        }
+
+        private DevicePacket GetPacketFromBytes(byte[] bytes)
+        {
+            DevicePacket str = new DevicePacket();
+
+            int size = Marshal.SizeOf(str);
+            IntPtr ptr = IntPtr.Zero;
+            try
+            {
+                ptr = Marshal.AllocHGlobal(size);
+
+                Marshal.Copy(bytes, 0, ptr, size);
+
+                str = (DevicePacket)Marshal.PtrToStructure(ptr, str.GetType());
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return str;
+        }
+
+        public override void Update()
+        {
+            if (!udpCreated)
+                return;
+
+            contents.Clear();
+
+            foreach (var kv in packets)
+            {
+                var packet = kv.Value;
+
+                company = packet.company;
+                DrawDevice(packet.device, packet.batteryLevel, packet.isCharging);
+            }
+
+            SetContents(contents.Values.ToArray());
+        }
+
+        private void DrawDevice(DeviceType device, int batteryLevel, bool isCharging)
+        {
+            contents.Add("HBI_DeviceBtn" + device, new Button("" + device, DeviceIcons.GetDeviceIcon(device, company, isCharging), new Action(() =>
+            {
+
+            })));
+
+            contents.Add("HBI_DeviceText" + device, new Header(batteryLevel + "%"));
+        }
+    }
+}
