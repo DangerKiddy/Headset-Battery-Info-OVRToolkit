be7ee353aa9523878c6722032d3a2600564392f3
DangerKiddy <dropspawn3rfree@gmail.com>
2024-07-12 19:03:07 +0700

    Added dynamic icons, new notification method, requests HBI refresh on startup

--- Main.cs
+++ Main.cs
@@ -5,6 +5,9 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
 
 namespace HeadsetBatteryInfo
 {
@@ -42,18 +45,20 @@ namespace HeadsetBatteryInfo
 
         public override void Start()
         {
+            Print("Initializing HBI...");
             InitModule("Headset Battery Info", File.ReadAllBytes("Plugins/HeadsetBatteryInfo/Images/batteryIcon.png"));
 
             DeviceIcons.Init();
 
             SetupDeviceDefaults();
+            SetupContents();
+
             InitUdp();
             ListenForData();
 
-            AddCustomBattery($"HBI_{DeviceType.Headset}", 1f, DeviceBatteryStates.Disconnected, DeviceBatteryIcons.HMD, null);
-            AddCustomBattery($"HBI_{DeviceType.ControllerLeft}", 1f, DeviceBatteryStates.Disconnected, DeviceBatteryIcons.LeftController, null);
-            AddCustomBattery($"HBI_{DeviceType.ControllerRight}", 1f, DeviceBatteryStates.Disconnected, DeviceBatteryIcons.RightController, null);
-            //SetContents(contents.Values.ToArray());
+            RequestRefresh();
+
+            Print("Initialized HBI");
         }
 
         private void SetupDeviceDefaults()
@@ -62,22 +67,29 @@ namespace HeadsetBatteryInfo
             {
                 device = DeviceType.Headset,
                 isCharging = false,
-                batteryLevel = 0,
+                batteryLevel = -1,
             };
 
             deviceData[DeviceType.ControllerLeft] = new DevicePacket
             {
                 device = DeviceType.ControllerLeft,
                 isCharging = false,
-                batteryLevel = 0,
+                batteryLevel = -1,
             };
 
             deviceData[DeviceType.ControllerRight] = new DevicePacket
             {
                 device = DeviceType.ControllerRight,
                 isCharging = false,
-                batteryLevel = 0,
+                batteryLevel = -1,
             };
+        }
+
+        private void SetupContents()
+        {
+            contents.Clear();
+
+            SetContents(contents.Values.ToArray());
         }
 
         private void InitUdp()
@@ -92,9 +104,17 @@ namespace HeadsetBatteryInfo
             catch
             {
                 contents.Add("HBI_Error", new Header("Failed to create listen socket!"));
+                Print("Failed to create UDP client for HBI!");
+                SendNotification("Headset Battery Info", "Failed to create UDP client for HBI!");
 
                 udpCreated = false;
             }
+        }
+
+        private void RequestRefresh()
+        {
+            var buffer = Encoding.ASCII.GetBytes("/hbi/requestUpdate");
+            udp.Send(buffer, buffer.Length, new IPEndPoint(IPAddress.Parse("127.0.0.1"), 28092));
         }
 
         private async void ListenForData()
@@ -105,15 +125,54 @@ namespace HeadsetBatteryInfo
 
                 if (data != null && data.Buffer.Length > 0)
                 {
-                    var packet = GetPacketFromBytes(data.Buffer);
-
-                    lock (this)
+                    if (data.Buffer[0] == 47)
                     {
-                        deviceData[packet.device] = packet;
+                        ProceedNotificationPacket(data.Buffer);
+                    }
+                    else
+                    {
+                        ProceedBatteryInfo(data.Buffer);
                     }
                 }
 
                 await Task.Delay(TimeSpan.FromMilliseconds(500));
+            }
+        }
+
+        private void ProceedNotificationPacket(byte[] buffer)
+        {
+            string msg = "";
+            bool readMsg = false;
+            for (int i = 1; i < buffer.Length; i++)
+            {
+                char c = (char)buffer[i];
+
+                if (c == 47)
+                {
+                    readMsg = true;
+                }
+                else if (readMsg)
+                {
+                    msg += c;
+                }
+            }
+
+            SendNotification("Headset Battery Info", msg);
+        }
+
+        private void ProceedBatteryInfo(byte[] buffer)
+        {
+            var packet = GetPacketFromBytes(buffer);
+
+            lock (this)
+            {
+                if (deviceData[packet.device].batteryLevel == -1)
+                {
+                    var icon = DeviceIcons.GetDeviceIcon(packet.device, packet.company, false);
+                    AddCustomBattery($"HBI_{packet.device}", packet.batteryLevel, DeviceBatteryStates.Connected, DeviceBatteryIcons.Custom, icon);
+                }
+
+                deviceData[packet.device] = packet;
             }
         }
 
@@ -149,9 +208,10 @@ namespace HeadsetBatteryInfo
                 foreach (var kv in deviceData)
                 {
                     var packet = kv.Value;
+                    if (packet.batteryLevel == -1)
+                        continue;
 
                     string key = $"HBI_{packet.device}";
-
                     float percent = packet.batteryLevel / 100f;
 
                     bool isConnected = packet.batteryLevel > 0;
